0b4d499b4a473e14212c58c85147e12148ac2fff
jStone721 <admin@jstone.gg>
2025-02-25 19:08:41 -0500

    Fix GitHub Actions permissions

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,16 +1,21 @@
-// Initialize Firebase using the global 'firebase' object provided by CDN
+// Import the functions you need from the SDKs you need
+import {initializeApp} from "firebase/app";
+import {getFirestore} from "firebase/firestore";
+import {getAuth} from "firebase/auth";
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyAkMccaMDOUcSrM7WcjwVZ3LP8rdWI1NAA",
   authDomain: "budgetplanner-952e3.firebaseapp.com",
   projectId: "budgetplanner-952e3",
-  storageBucket: "budgetplanner-952e3.appspot.com",
+  storageBucket: "budgetplanner-952e3.firebasestorage.app",
   messagingSenderId: "491817221899",
   appId: "1:491817221899:web:17352c66098004772b2c0e",
 };
 
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
 
-const auth = firebase.auth();
-const db = firebase.firestore();
+export {auth, db};
