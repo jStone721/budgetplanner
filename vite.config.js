92a57465a550bb964ed0a2258e09f6a8521c09f2
jStone721 <admin@jstone.gg>
2025-02-25 19:30:32 -0500

    Fix GitHub Actions permissions

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,18 @@
 import {defineConfig} from "vite";
+import {resolve} from "path";
 
 export default defineConfig({
-  base: "/budgetplanner/", // Replace with your repo name
+  base: "/budgetplanner/", // GitHub Pages repo name
+  resolve: {
+    alias: {
+      crypto: "crypto-browserify",
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+      },
+    },
+  },
 });
